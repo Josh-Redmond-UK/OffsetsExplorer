0c81719d4a173c43b4f3c05e9db603b54a4d3762
Josh Redmond <jr725@exeter.ac.uk>
2023-06-23 09:09:05 +0100

    Initial Commit

--- assets/js/globeDisplay.js
+++ assets/js/globeDisplay.js
@@ -7,6 +7,10 @@ var names = ['Guatemala', 'Tanzania', 'Hunan', 'Darkwoods']
 
 
 
+var map = L.map('map').setView([0, 0], 1);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 
 fetch('../PointsLatLon.csv').then(response => response.text())
@@ -15,12 +19,24 @@ fetch('../PointsLatLon.csv').then(response => response.text())
      var projectCSV = Papa.parse(data, {header:true});
      console.log(projectCSV.data);
      var numProj = projectCSV.data.length;
+     var dataArray = new Array();
      var names = []
      var lats = []
      var lons = [] 
 
      for (i=0; i<numProj; i++){
         var _p = projectCSV.data[i]
+        var marker = L.marker([_p['Latitude'], _p['Longitude']]).addTo(map);
+        console.log(_p['Project Title'])
+        marker.bindPopup(`${_p['Project Title']}<br><br>Total Credits Retired: ${_p['Credits Retired Total']}<br><br>Credits Retired by Shell: ${_p['Credits Retired Company']}`)
+        dataArray.push({
+            title:_p['Project Title'],
+            lat:_p['Latitude'],
+            lng:_p['Longitude']
+           
+
+
+        })
         names.push(_p['Project Title'])
         lats.push(_p['Latitude'])
         lons.push(_p['Longitude'])
@@ -28,42 +44,7 @@ fetch('../PointsLatLon.csv').then(response => response.text())
 
 
      }
-     var labelData = {}
-     lats = lats.slice(0,-3)
-     lons = lons.slice(0,-3)
-     names = names.slice(0,-3)
-     labelData['lat'] = lats
-     labelData['lng'] = lons
-     labelData['text'] = names
-    const N = 300;
-    const gData = [...Array(N).keys()].map(() => ({
-      lat: (Math.random() - 0.5) * 180,
-      lng: (Math.random() - 0.5) * 360,
-      size: Math.random() / 3,
-      color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-    }));
-     const myGlobe = Globe();
-    myGlobe(document.getElementById('globeViz'))
-    .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-    .backgroundColor('rgba(0,0,0,0.1)')
-    .width(screen.width/2)
-    .labelsData(projectCSV.data)
-
-     
-
-
-
-     console.log(lats)
-     console.log(labelData)
-     myGlobe.pointsData([])
-     myGlobe.pointLat(0)
-     myGlobe.pointLng(1)
-
-
-    myGlobe.controls().autoRotate = true;
-    myGlobe.controls().autoRotateSpeed = -0.45;
-
-
+  
      
   });
 
